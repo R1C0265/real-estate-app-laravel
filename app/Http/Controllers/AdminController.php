374bf80e5e1e441cde213782e353646a03fa76b3
= <=>
2026-01-26 00:53:34 +0200

    Created Logout for admin. via the person icon on the top right corner

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
 
 class AdminController extends Controller
 {
@@ -10,4 +14,19 @@ class AdminController extends Controller
         return view('admin.index');
     }
     //End Metho
+
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
 }
